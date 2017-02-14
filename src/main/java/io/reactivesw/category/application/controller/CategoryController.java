3a880796e250db9397669954cbac45fa5dca274c
umasuo <liuquan89@gmail.com>
2017-02-14 15:06:36 +0800

    add controller

---
+++ src/main/java/io/reactivesw/category/application/controller/CategoryController.java
@@ -0,0 +1,147 @@
+package io.reactivesw.category.application.controller;
+
+import io.reactivesw.category.application.model.CategoryDraft;
+import io.reactivesw.category.application.model.CategoryView;
+import io.reactivesw.category.application.model.PagedQueryResult;
+import io.reactivesw.category.application.model.QueryConditions;
+import io.reactivesw.category.domain.service.CategoryService;
+import io.reactivesw.category.infrastructure.Router;
+import io.reactivesw.category.infrastructure.util.UpdateRequest;
+import io.reactivesw.category.infrastructure.validator.CategoryNameValidator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+
+/**
+ * Created by Davis on 16/11/18.
+ */
+@ApiModel
+@RestController
+public class CategoryController {
+  /**
+   * log.
+   */
+  private static final Logger LOG = LoggerFactory.getLogger(CategoryController.class);
+
+  /**
+   * CategoryEntity Service.
+   */
+  @Autowired
+  private transient CategoryService categoryService;
+
+  /**
+   * Create category category.
+   *
+   * @param categoryDraft the draft
+   * @return the category
+   */
+  @ApiOperation(value = "Create CategoryEntity")
+  @PostMapping(Router.CATEGORY_ROOT)
+  public CategoryView createCategory(@RequestBody
+                                     @ApiParam(value = "CategoryEntity Draft", required = true)
+                                     @Valid CategoryDraft categoryDraft) {
+    LOG.debug("create category : {}", categoryDraft.toString());
+
+    CategoryNameValidator.validateNull(categoryDraft);
+
+    CategoryView category = categoryService.createCategory(categoryDraft);
+
+    LOG.debug("end createCategory, saved category is {}", category.toString());
+
+    return category;
+  }
+
+  /**
+   * Delete category.
+   *
+   * @param version the version
+   */
+  @ApiOperation(value = "Delete CategoryEntity By Id")
+  @DeleteMapping(value = Router.CATEGORY_WITH_ID)
+  public void deleteCategory(@PathVariable(value = Router.CATEGORY_ID)
+                             @ApiParam(value = "CategoryEntity ID", required = true)
+                                 String id,
+                             @RequestParam
+                             @ApiParam(value = "CategoryEntity Version", required = true)
+                                 Integer version) {
+    LOG.debug("enter deleteCategory, id is {}, version is {}", id, version);
+
+    categoryService.deleteCategory(id, version);
+
+    LOG.debug("end deleteCategory, id is {}, version is {}", id, version);
+  }
+
+  /**
+   * Update category category.
+   *
+   * @param id            the id
+   * @param updateRequest the fields
+   * @return the category
+   */
+  @ApiOperation(value = "Update CategoryEntity")
+  @PutMapping(Router.CATEGORY_WITH_ID)
+  public CategoryView updateCategory(@PathVariable(value = Router.CATEGORY_ID)
+                                     @ApiParam(value = "CategoryEntity ID", required = true)
+                                         String id,
+                                     @RequestBody
+                                     @ApiParam(value = "CategoryEntity Update Fields", required =
+                                         true)
+                                     @Valid UpdateRequest updateRequest) {
+    LOG.debug("enter updateCategory,id is {}, update request is {}", id, updateRequest.toString());
+
+    CategoryView result = categoryService.updateCategory(id, updateRequest.getVersion(),
+        updateRequest.getActions());
+
+    LOG.debug("end updateCategory, updated Category is {}", result.toString());
+
+    return result;
+  }
+
+  /**
+   * Gets category by id.
+   *
+   * @param id the id
+   * @return the category by id
+   */
+  @ApiOperation(value = "Get CategoryEntity By Id")
+  @GetMapping(Router.CATEGORY_WITH_ID)
+  public CategoryView getCategoryById(@PathVariable(value = Router.CATEGORY_ID)
+                                      @ApiParam(value = "CategoryEntity ID", required = true)
+                                          String id) {
+    LOG.debug("enter getCategoryById, id is {}", id);
+    CategoryView category = categoryService.getCategoryById(id);
+    LOG.debug("end getCategoryById, get category : {}", category.toString());
+    return category;
+  }
+
+  /**
+   * Query category list.
+   *
+   * @return the list
+   */
+  @ApiOperation(value = "Get Categories")
+  @GetMapping(Router.CATEGORY_ROOT)
+  public PagedQueryResult<CategoryView> queryCategories(QueryConditions query) {
+    LOG.debug("enter queryCategories, query parameters: {}", query.toString());
+
+    PagedQueryResult<CategoryView> result = categoryService.queryCategories(query);
+
+    LOG.debug("end queryCategories, query result is : {}", result.toString());
+
+    return result;
+  }
+}
