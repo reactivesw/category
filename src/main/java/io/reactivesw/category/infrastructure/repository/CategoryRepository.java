a7a26c230c4f0a037fc3f26e16721c5e20a520b4
umasuo <liuquan89@gmail.com>
2017-02-14 14:27:47 +0800

    add category repository

---
+++ src/main/java/io/reactivesw/category/infrastructure/repository/CategoryRepository.java
@@ -0,0 +1,51 @@
+package io.reactivesw.category.infrastructure.repository;
+
+import io.reactivesw.category.domain.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by umasuo on 16/11/22.
+ */
+public interface CategoryRepository extends JpaRepository<Category, String>,
+    JpaSpecificationExecutor {
+  /**
+   * Query category ids by ancestor id list.
+   *
+   * @param ancestorId the ancestorId
+   * @return the list
+   */
+  @Query(value = "select c from CategoryEntity c where ?1 member of c.ancestors")
+  List<Category> querySubCategoriesByAncestorId(String ancestorId);
+
+  /**
+   * Query category by parent list.
+   *
+   * @param parentId the parent id
+   * @return the list
+   */
+  List<Category> queryCategoryByParent(String parentId);
+
+  /**
+   * Delete category by id.
+   *
+   * @param Ids the ids
+   */
+  @Modifying
+  @Transactional
+  @Query(value = "delete from CategoryEntity c where c.id in ?1")
+  void deleteCategoryById(List<String> Ids);
+
+  /**
+   * Find category by slug.
+   *
+   * @param slug the slug
+   * @return the category entity
+   */
+  Category findCategoryBySlug(String slug);
+}
