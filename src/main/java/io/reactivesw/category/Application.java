f549e0cb08229921084fc938475e4cfd58ec7159
Davis <davis.dai@go6d.com>
2017-03-07 20:08:44 +0800

    add comment

--- src/main/java/io/reactivesw/category/Application.java
+++ src/main/java/io/reactivesw/category/Application.java
@@ -9,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "io.reactivesw")
 public class Application {
 
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
