19f29a956bac278a09de713430c7766bcc3cf11b
Davis <davis.dai@go6d.com>
2017-03-08 15:31:15 +0800

    edit health controller

--- src/main/java/io/reactivesw/category/application/controller/HealthController.java
+++ src/main/java/io/reactivesw/category/application/controller/HealthController.java
@@ -2,6 +2,8 @@ package io.reactivesw.category.application.controller;
 
 import io.reactivesw.category.infrastructure.Router;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,11 @@ import sun.net.www.http.HttpClient;
 public class HealthController {
 
   /**
+   * log.
+   */
+  private static final Logger LOG = LoggerFactory.getLogger(HealthController.class);
+
+  /**
    * service name.
    */
   @Value("${spring.application.name}")
@@ -29,8 +36,13 @@ public class HealthController {
    */
   @GetMapping(Router.CATEGORY_HELTH_CHECK)
   public String healthCheck() {
+    LOG.debug("enter healthCheck");
+
     RestTemplate restTemplate = new RestTemplate();
     Object object = restTemplate.getForObject("https://api.github.com", Object.class);
+
+    LOG.debug("get response : {}", object.toString());
+
     return serviceName + ", system time: " + System.currentTimeMillis() + " - " + object.toString();
   }
 }
