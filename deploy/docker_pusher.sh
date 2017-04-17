f44b4575d01d8bb8152f9b94564d4524fc86d869
Davis <davis.dai@go6d.com>
2017-04-17 21:00:22 +0800

    edit docker script

--- deploy/docker_pusher.sh
+++ deploy/docker_pusher.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo "------------------ push docker image -----------------"
+echo ${TRAVIS_BRANCH}
 if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "addEvent" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD";
   docker push reactivesw/category;
 fi
+echo "------------------ end push docker image -------------"
