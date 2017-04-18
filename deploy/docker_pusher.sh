18222ebfd566fed90bfcac252b21ec986e21a704
Davis <davis.dai@go6d.com>
2017-04-18 09:51:42 +0800

    edit docker push script

--- deploy/docker_pusher.sh
+++ deploy/docker_pusher.sh
@@ -5,12 +5,12 @@ set -e
 echo "============= push docker image ====================================="
 echo ${TRAVIS_BRANCH}
 echo "====================================================================="
-export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo $TRAVIS_BRANCH; else
     echo $TRAVIS_PULL_REQUEST_BRANCH;
     fi)
 echo "====================================================================="
-if [ "$BRANCH" == "master" ] || [ "$BRANCH" == "addEvent" ]; then
+if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "addEvent" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD";
   docker push reactivesw/product;
 fi
