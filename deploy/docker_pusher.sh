5eae4fb56c87d66a267a2784c2937e3df313fdac
Davis <davis.dai@go6d.com>
2017-04-18 10:50:34 +0800

    change version

--- deploy/docker_pusher.sh
+++ deploy/docker_pusher.sh
@@ -9,8 +9,9 @@ export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo $TRAVIS_BRANCH; else
     echo $TRAVIS_PULL_REQUEST_BRANCH;
     fi)
+echo ${BRANCH}
 echo "====================================================================="
-if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "addEvent" ]; then
+if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "dev" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD";
   docker push reactivesw/category;
 fi
